1b630fe63b4a93f2555392147f0305b20921ce89
caicoso <69042973+caicoso@users.noreply.github.com>
2020-08-28 07:58:21 -0300

    Add files via upload
    bas

---
+++ Inicio_Control.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+    
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Entrando na Home da Minha Aplicação");
+		return "home";
+	}
+	
+}
